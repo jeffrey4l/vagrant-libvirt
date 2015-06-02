dc9333f1a261e6a7290543e786277dfaaf5cf113
Michael Kerrin <michael.kerrin@hp.com>
2015-06-02 10:00:38 +0100

    Allow use of virtio-scsi driver for SCSI devices
    For better performance should allow enabling the virtio-scsi
    model on the scsi controller in VM's.

--- lib/vagrant-libvirt/config.rb
+++ lib/vagrant-libvirt/config.rb
@@ -83,6 +83,7 @@ module VagrantPlugins
       attr_accessor :machine_arch
       attr_accessor :machine_virtual_size
       attr_accessor :disk_bus
+      attr_accessor :disk_controller_model
       attr_accessor :disk_device
       attr_accessor :nic_model_type
       attr_accessor :nested
@@ -203,6 +204,7 @@ module VagrantPlugins
         @machine_arch      = UNSET_VALUE
         @machine_virtual_size = UNSET_VALUE
         @disk_bus          = UNSET_VALUE
+        @disk_controller_model = UNSET_VALUE
         @disk_device       = UNSET_VALUE
         @nic_model_type    = UNSET_VALUE
         @nested            = UNSET_VALUE
@@ -681,6 +683,7 @@ module VagrantPlugins
         @machine_arch = nil if @machine_arch == UNSET_VALUE
         @machine_virtual_size = nil if @machine_virtual_size == UNSET_VALUE
         @disk_bus = 'virtio' if @disk_bus == UNSET_VALUE
+        @disk_controller_model = 'auto' if @disk_controller_model == UNSET_VALUE
         @disk_device = 'vda' if @disk_device == UNSET_VALUE
         @nic_model_type = nil if @nic_model_type == UNSET_VALUE
         @nested = false if @nested == UNSET_VALUE
